24ea1755c355a6041a8b025519bfb9c8f3e8f531
Pamela Raiane <pamelaprpn@gmail.com>
2024-05-20 20:32:29 -0300

    feat: structure project with styledComponents

---
+++ src/components/CarrosselHome/index.jsx
@@ -0,0 +1,9 @@
+import * as S from './style';
+
+export default function CarrosselHome(){
+  return(
+    <>
+
+    </>
+  )
+}
---
+++ src/pages/Home/components/Menu/index.jsx
@@ -0,0 +1,18 @@
+import * as S from './style';
+
+export default function Menu(){
+  return(
+    <>
+      <div>
+        <ul>
+          <li><a>Séries</a></li>
+          <li><a>Filmes</a></li>
+          <li><a>Originais</a></li>
+          <li><a>Lançamentos</a></li>
+          <li><a>Últimos dias</a></li>
+          <li><a>Em breve</a></li>
+        </ul>
+      </div>
+    </>
+  )
+}
